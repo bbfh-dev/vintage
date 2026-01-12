cb0d0f536895bdbb842133dcb0a29d3cc7d82763
BubbleFish <daforsastudia@gmail.com>
2026-01-12 21:41:49 +0300

    feat: Export the resource pack

--- mime/project.go
+++ mime/project.go
@@ -8,19 +8,24 @@ import (
 	"github.com/bbfh-dev/mime/cli"
 	"github.com/bbfh-dev/mime/mime/errors"
 	"github.com/bbfh-dev/mime/mime/minecraft"
+	cp "github.com/otiai10/copy"
 )
 
 type Project struct {
-	Meta     *minecraft.PackMcmeta
-	has_icon bool
-	task_err error
+	BuildDir      string
+	Meta          *minecraft.PackMcmeta
+	has_icon      bool
+	has_resources bool
+	task_err      error
 }
 
 func New(mcmeta *minecraft.PackMcmeta) *Project {
 	return &Project{
-		Meta:     mcmeta,
-		has_icon: false,
-		task_err: nil,
+		BuildDir:      cli.Main.Options.Output,
+		Meta:          mcmeta,
+		has_icon:      false,
+		has_resources: false,
+		task_err:      nil,
 	}
 }
 
@@ -36,6 +41,7 @@ func (project *Project) Build() error {
 	project.do(project.checkBuildDir)
 	project.do(project.clearBuildDir)
 	project.do(project.detectPackIcon)
+	project.do(project.createResourcePack)
 
 	if project.task_err != nil {
 		return project.task_err
@@ -52,22 +58,22 @@ func (project *Project) do(task func() error) {
 }
 
 func (project *Project) checkBuildDir() error {
-	dir, _ := filepath.Abs(cli.Main.Options.Output)
-	cli.LogInfo(false, "Checking build directory: %s", dir)
+	project.BuildDir, _ = filepath.Abs(project.BuildDir)
+	cli.LogInfo(false, "Checking build directory: %s", project.BuildDir)
 
-	stat, err := os.Stat(dir)
+	stat, err := os.Stat(project.BuildDir)
 	if err != nil {
 		if os.IsNotExist(err) {
 			cli.LogDebug(true, "Directory doesn't exist yet. Skipping checks")
 			return nil
 		}
-		return errors.NewError(errors.ERR_IO, dir, err.Error())
+		return errors.NewError(errors.ERR_IO, project.BuildDir, err.Error())
 	}
 
 	if !stat.IsDir() {
 		return errors.NewError(
 			errors.ERR_VALID,
-			dir,
+			project.BuildDir,
 			"build output is a file",
 		)
 	}
@@ -96,6 +102,39 @@ func (project *Project) detectPackIcon() error {
 	if !os.IsNotExist(err) {
 		cli.LogInfo(false, "Found 'pack.png'")
 		project.has_icon = true
+	}
+
+	return nil
+}
+
+func (project *Project) createResourcePack() error {
+	_, err := os.Stat("assets")
+	if os.IsNotExist(err) {
+		cli.LogDebug(false, "No resource pack found")
+		return nil
+	}
+
+	cli.LogInfo(false, "Creating a resource pack")
+	project.has_resources = true
+
+	err = os.MkdirAll(filepath.Join(project.BuildDir, "resource_pack"), os.ModePerm)
+	if err != nil {
+		return errors.NewError(errors.ERR_IO, project.BuildDir, err.Error())
+	}
+
+	cli.LogInfo(true, "Copying 'assets/'")
+	path := filepath.Join(project.BuildDir, "resource_pack", "assets")
+	err = cp.Copy("assets", path)
+	if err != nil {
+		return errors.NewError(errors.ERR_IO, path, err.Error())
+	}
+
+	if project.has_icon {
+		path := filepath.Join(project.BuildDir, "resource_pack", "pack.png")
+		err = cp.Copy("pack.png", path)
+		if err != nil {
+			return errors.NewError(errors.ERR_IO, path, err.Error())
+		}
 	}
 
 	return nil
